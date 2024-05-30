7cbf9a14ec0eafcd3e542412803b773cdb156f90
nakul hackerji <155868717+nakuldkdhacker0026@users.noreply.github.com>
2024-05-30 09:17:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,19 +13,19 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 API_ID = "27922619"
 API_HASH = "e3426da4a617414c6cdf62df6f5524b3"
 CMD_HNDLR = getenv("CMD_HNDLR", default=".")
-HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", "innocentxspam")
+HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 
-BOT_TOKEN = getenv("BOT_TOKEN", default="6511740403:AAHGJoyxJngS_ewWEPCpzl4-zch8mDRVj9Q")
-BOT_TOKEN2 = getenv("BOT_TOKEN2", default="7048430778:AAG_O7YmnLGd2cCqUTtrkpMV68dn3Cmpnvw")
-BOT_TOKEN3 = getenv("BOT_TOKEN3", default="7071102149:AAEESaHQgwVwrDmP53QXB88tgm6V6xFMr8Q")
-BOT_TOKEN4 = getenv("BOT_TOKEN4", default="6422267508:AAEBgbHrBiAj7pgxrPHntb6W6rJll_po4F4")
-BOT_TOKEN5 = getenv("BOT_TOKEN5", default="7103801898:AAEq7mEfQzspoVm-sx_EymJ418ipE043y5U")
-BOT_TOKEN6 = getenv("BOT_TOKEN6", default="7029167424:AAGdndM9LsvQGewRYaYghEczt7dMDUbD9ak")
-BOT_TOKEN7 = getenv("BOT_TOKEN7", default="6728619918:AAESKf3SxAmHXqoEEVl3adaRQ1LZRiR2qJ8")
-BOT_TOKEN8 = getenv("BOT_TOKEN8", default="7005367689:AAEH6KfWpisjmvazYZHzHdY2gguh7IQukEc")
-BOT_TOKEN9 = getenv("BOT_TOKEN9", default="6956089230:AAH8SEYTPMgTZ_to22NB79x7wnkaNbbksc8")
-BOT_TOKEN10 = getenv("BOT_TOKEN10", default="6458334654:AAEz1MZGbu1VowQXIKde2-xjLigyJ4P-yyw")
+BOT_TOKEN = getenv("BOT_TOKEN", default=None)
+BOT_TOKEN2 = getenv("BOT_TOKEN2", default=None)
+BOT_TOKEN3 = getenv("BOT_TOKEN3", default=None)
+BOT_TOKEN4 = getenv("BOT_TOKEN4", default=None)
+BOT_TOKEN5 = getenv("BOT_TOKEN5", default=None)
+BOT_TOKEN6 = getenv("BOT_TOKEN6", default=None)
+BOT_TOKEN7 = getenv("BOT_TOKEN7", default=None)
+BOT_TOKEN8 = getenv("BOT_TOKEN8", default=None)
+BOT_TOKEN9 = getenv("BOT_TOKEN9", default=None)
+BOT_TOKEN10 = getenv("BOT_TOKEN10", default=None)
 
 SUDO_USERS = list(map(lambda x: int(x), getenv("SUDO_USERS", default="6819558758").split()))
 for x in ALTRON:
